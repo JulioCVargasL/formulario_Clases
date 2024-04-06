0f46b2ae0972e7c535e9a9d59391ad301e39fe96
JulioVargas <2.0.jcvl92@gmail.com>
2024-04-06 18:44:29 -0500

    Guadado de Emergencia

--- formulario_Clases/formulario_Clases/forms.py
+++ formulario_Clases/formulario_Clases/forms.py
@@ -1,3 +1,39 @@
 from django import forms
 
-class 
+SELECTOR_URGENCIA = {
+  "1":"Bajo",
+  "2":"Medio",
+  "3":"Alto",
+}
+
+class Contact(forms.Form):
+
+  first_name  = forms.CharField(
+    label="Nombre", 
+    max_length=50,
+    widget=forms.TextInput(attrs={'class': 'input'}),
+    )
+  
+  last_name   = forms.CharField(
+    label="Apellidos", 
+    max_length=50,
+    widget=forms.TextInput(attrs={'class': 'input'})
+    )
+  
+  asunto      = forms.CharField(
+    label="Asunto",
+    max_length=50,
+    widget=forms.TextInput(attrs={'class': 'input'})
+    )
+  
+  mensaje     = forms.CharField( 
+    max_length=50,
+    label="Mensaje",
+    widget=forms.TextInput(attrs={'class': 'input'})
+    )
+  
+  prioridad   = forms.ChoiceField(
+    choices=SELECTOR_URGENCIA,
+    widget=forms.Select(attrs={'class': 'input'})
+  )
+  
--- formulario_Clases/formulario_Clases/views.py
+++ formulario_Clases/formulario_Clases/views.py
@@ -1,6 +1,20 @@
 from django.http      import HttpResponse 
 from django.shortcuts import render
+from .forms import Contact
 
 def form(request):
-  return render(request, 'formulario.html', {})
+  # GET
+  if request.method == 'GET':
+    form = Contact({
+      'first_name': 'Ejemplo: Julio Cesar',
+      'last_name' : 'Ejemplo: Vargas Lopez',
+      'asunto'    : 'Ejemplo: Inquietud',
+      'mensaje'   : 'Ejemplo: Hola mundo !',
+    })
+    return render(request, 'formulario.html', {
+    'formulario': form
+    })
+  # POST
+  if request.method == 'POST':
+    return HttpResponse ("Lo logramos enviar")
 
